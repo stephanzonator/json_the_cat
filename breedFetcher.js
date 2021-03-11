1d1d8dd20e70b9909a6a88edc08c29646e36ed0f
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-11 23:12:52 +0000

    fixed errors, eslint-ed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const breed = process.argv.slice(2)[0];
 const breedFormat = breed.toLowerCase();
-const breedCode = breedFormat[0] + breedFormat[1] + breedFormat[2] + breedFormat[3]
+const breedCode = breedFormat[0] + breedFormat[1] + breedFormat[2] + breedFormat[3];
 const request = require('request');
 const pageName = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedCode}`;
 
@@ -9,10 +9,22 @@ const pageName = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedCo
 
 request(pageName, (error, response, body) => {
   
-  console.log('error:', error); // Print the error if one occurred
+  if (error !== null) {
+    console.log("Error: couldn't find website. Please check URL and try again.");
+  } else {
+    const data = JSON.parse(body);
+    console.log("body length:", body.length);
+    if (body.length > 2) {  //for some reason the length of an empty array is 2. this checks if larger than empty array
+      console.log("\n description:", data[0]["breeds"][0]["description"]);
+    } else {
+      console.log("Breed not found in data.");
+    }
+    
+  }
+  // console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
-  // const data = JSON.parse(body);
+  
   // console.log("\n description:", data[0]["breeds"][0]["description"]);
   
 
